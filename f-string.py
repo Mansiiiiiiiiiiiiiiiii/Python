e9b2ee732f261a63cd9e366917822492c587a3ad
Mansiiiiiiiiiiiiiiiii <mansi15094@gmail.com>
2024-08-13 00:49:59 +0530

    Python

---
+++ f-string.py
@@ -0,0 +1,10 @@
+letter = "Hay my name is {1} and I am from {0}"
+country = "India"
+name = "Mansi"
+print(letter.format(country, name))
+print(f"Hay my name is {name} and I am from {country}")
+price = 49.09999
+txt = f"For only {price: 2f} dollars!"
+print(txt)
+print(f"{2*30}")
+print(type(f"{2*30}"))
